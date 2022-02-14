2ed87a8fc5f6a46a317a9e1783fe1c02811b534b
Florian Hartl <florian@hartl-home.eu>
2022-02-14 17:45:34 +0100

    multiple colors

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  //console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log("Plugin up and running");
 });
