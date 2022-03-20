364183a4bf4253e6ee639501d33b28e7cfb4aa39
Florian Hartl <florian@hartl-home.eu>
2022-03-20 15:23:31 +0100

    new defaults

--- background.js
+++ background.js
@@ -12,8 +12,8 @@ chrome.runtime.onInstalled.addListener(() => {
 //set default settings
 chrome.storage.sync.set({'PDactivationStatus': true}, function() {});
 chrome.storage.sync.set({'PDsetBGColor': true}, function() {});
-chrome.storage.sync.set({'PDblockEntries': false}, function() {});
-chrome.storage.sync.set({'PDlanguage': "english"}, function() {});
+chrome.storage.sync.set({'PDblockEntries': true}, function() {});
+chrome.storage.sync.set({'PDlanguage': "german"}, function() {});
 chrome.storage.sync.set({'PDcurrentSiteInfos': ["PD_Default", "safe", "whitelist"]}, function() {});
 
 var message = "testmessage";
